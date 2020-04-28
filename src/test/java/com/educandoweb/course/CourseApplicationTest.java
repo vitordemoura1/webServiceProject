e9ec6a70132987460e64fde0a24e248079ee5068
Vitor de Moura <moura.vitor20@hotmail.com>
2020-04-27 21:48:12 -0300

    Project created

---
+++ src/test/java/com/educandoweb/course/CourseApplicationTest.java
@@ -0,0 +1,14 @@
+package com.educandoweb.course;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CourseApplicationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
